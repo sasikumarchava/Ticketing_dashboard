8ed7c9d41195bfb53d8c12e2e95c4f90b0689a04
SASI CHAVA <88151734+sasikumarchava@users.noreply.github.com>
2024-07-19 12:49:53 +0530

    Update ticket.js

--- src/api/ticket.js
+++ src/api/ticket.js
@@ -1,38 +1,38 @@
 // // api/tickets.js
-// import axios from 'axios';
-
-// export const fetchTickets = async () => {
-//   const response = await axios.get('http://localhost:5000/tickets');
-//   return response.data;
-// };
-
-// const BASE_URL = 'http://localhost:5000';
-
-// export const saveTicketsApi = async (tickets) => {
-//   try {
-//     const response = await axios.post(`${BASE_URL}/tickets`, tickets);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error(`Error saving tickets: ${error.message}`);
-//   }
-
-// };
-
 import axios from 'axios';
 
-const BASE_URL = "ticket-27ydrs7nw-sasikumarchavas-projects.vercel.app";
-
 export const fetchTickets = async () => {
-  const response = await axios.get(`${BASE_URL}/api/tickets`);
+  const response = await axios.get('http://localhost:5000/tickets');
   return response.data;
 };
 
+const BASE_URL = 'http://localhost:5000';
+
 export const saveTicketsApi = async (tickets) => {
   try {
-    const response = await axios.post(`${BASE_URL}/api/tickets`, tickets);
+    const response = await axios.post(`${BASE_URL}/tickets`, tickets);
     return response.data;
   } catch (error) {
     throw new Error(`Error saving tickets: ${error.message}`);
   }
+
 };
+
+// import axios from 'axios';
+
+// const BASE_URL = "ticket-27ydrs7nw-sasikumarchavas-projects.vercel.app";
+
+// export const fetchTickets = async () => {
+//   const response = await axios.get(`${BASE_URL}/api/tickets`);
+//   return response.data;
+// };
+
+// export const saveTicketsApi = async (tickets) => {
+//   try {
+//     const response = await axios.post(`${BASE_URL}/api/tickets`, tickets);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(`Error saving tickets: ${error.message}`);
+//   }
+// };
 
